36bbed1e2c65b70db7807655546f72e46d6694bb
juanma rivero <110467078+Juanm4R@users.noreply.github.com>
2023-08-08 19:43:52 -0300

    aaaaaaaaaaaaaaa gus

--- pages/api/authMiddleware.ts
+++ pages/api/authMiddleware.ts
@@ -3,16 +3,30 @@ import jwt from 'jsonwebtoken';
 
 const key = process.env.SECRETKEY as string;
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  const token = req.cookies.token
-  try{
-  const tokenExists = jwt.verify(token as string,key)
-  return res.status(200).json({message:'Podes Entrar'});
+export interface DecodedToken {
+  email: string;
+}
+
+declare module 'next' {
+  interface NextApiRequest {
+    user?: DecodedToken; 
   }
-  catch{
-    return res.status(405).json({ error: 'No podes Entrar' });
-  }
+}
+
+export default function authMiddleware(handler: Function) {
+  return async (req: NextApiRequest, res: NextApiResponse) => {
+    const token = req.cookies.token;
+
+    if (!token) {
+      return res.redirect('/login');
+    }
+
+    try {
+      const decoded = jwt.verify(token, key) as DecodedToken;
+      req.user = decoded;
+      return handler(req, res);
+    } catch (error) {
+      return res.redirect('/login');
+    }
+  };
 }
--- pages/api/music.ts
+++ pages/api/music.ts
@@ -1,30 +1,32 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { PrismaClient } from '@prisma/client';
+import authMiddleware from './authMiddleware';
 
 const prisma = new PrismaClient();
 
-export default async function handler(
+async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   if (req.method === 'POST') {
     const { email } = req.body;
-  
+
     try {
       const UserHistExist = await prisma.music.findUnique({ 
         where: { email : email },
         select: { genre : true, file : true, name : true }
-     });
-     if (UserHistExist)
-     return res.status(200).json(UserHistExist);
-     else{
-        return res.status(200).json({error:'no se encontro al usuario'})
-     }
-    }
-    catch (error) {
-      return res.status(400).json({ error: 'Ocurrió un error al recuperar la musica del usuario' });
+      });
+      if (UserHistExist) {
+        return res.status(200).json(UserHistExist);
+      } else {
+        return res.status(200).json({ error: 'no se encontro al usuario' });
+      }
+    } catch (error) {
+      return res.status(400).json({ error: 'Ocurrió un error al recuperar la música del usuario' });
     }
   }
 
   return res.status(405).json({ error: 'Método no permitido' });
 }
+
+export default authMiddleware(handler);
