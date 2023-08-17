2b514218ef16daa3d64e3f8e5b92ba9feccadf64
AgusFeld <101400409+AgusFeld@users.noreply.github.com>
2023-08-17 10:27:58 -0300

    Home empezada

--- pages/home.tsx
+++ pages/home.tsx
@@ -12,7 +12,22 @@ const Home: React.FC = () => {
                     <img className={styles.logo} src="Logo.png"></img>
                 </div>
                 <div className={styles.abajo}>
-                    <div className={styles.filtro}></div>
+                    <div className={styles.filtro}>
+                        <div className={styles.text}>
+                            <div className={styles.a}>Filtros</div>
+                        </div>
+                        <div className={styles.seleccionar}>
+                            <div className={styles.elecciones}>
+                                <h1 className={styles.b}>Jazz</h1>
+                                <h1 className={styles.b}>Blues</h1>
+                                <h1 className={styles.b}>Disco</h1>
+                                <h1 className={styles.b}>Rock</h1>
+                                <h1 className={styles.b}>Reggae</h1>
+                            </div>
+                            <div className={styles.tick}></div>
+                        </div>
+                    </div>
+                    <div className={styles.buscador}></div>
                 </div>
             </div>
         </div>
