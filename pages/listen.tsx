0b3806bf7c5b0e5e1255b09aad3a0e4445ea59d1
AgusFeld <101400409+AgusFeld@users.noreply.github.com>
2023-09-07 15:52:11 -0300

    listen modificado

--- pages/listen.tsx
+++ pages/listen.tsx
@@ -4,6 +4,12 @@ import styles from "./listen.module.css";
 import ViniloComponent from "./viniloComponent";
 
 const Listen: React.FC = () => {
+    
+        const [Play, Pause] = useState(false);
+      
+        const togglePlayPause = () => {
+          Pause(!Play);
+        };
 
     return(
         <div className={styles.container}>
@@ -15,8 +21,13 @@ const Listen: React.FC = () => {
                 <img className={styles.loop} src="Loop.svg"/>
                 <img className={styles.random} src="Random.svg"/>
                 <div className={styles.interact}>
+                    <div className={styles.blank}></div>
                     <img className={styles.img} src="Back.svg"/>
-                    <img className={styles.play} src="Play.svg"/>
+                    <img
+                        className={styles.play}
+                        src={Play ? 'Pause.svg' : 'Play.svg'}
+                        onClick={togglePlayPause}
+                    />
                     <img className={styles.img} src="Forward.svg"/>
                 </div>
             </div>
