45d3f93b03ded75559fc0bd70175537f28d52f84
AgusFeld <101400409+AgusFeld@users.noreply.github.com>
2023-09-13 15:58:07 -0300

    editor con valores guardados

--- pages/editor.tsx
+++ pages/editor.tsx
@@ -3,7 +3,7 @@ import styles from "./editor.module.css";
 
 const Editor: React.FC = () => {
   const [selectedRows, setSelectedRows] = useState<Array<number | null>>(
-    Array.from({ length: 10 }, () => null)
+    Array.from({ length: 40 }, () => null) // Adjust the length to match the number of columns
   );
 
   // Define the available sounds for each row
@@ -16,6 +16,17 @@ const Editor: React.FC = () => {
     setSelectedRows(newSelectedRows);
   };
 
+  // Function to save the selected cells
+  const saveSelectedCells = () => {
+    // Filter out null values (unselected cells)
+    const selectedCells = selectedRows.filter((rowIndex) => rowIndex !== null);
+
+    // Now, you can send the selectedCells data to your backend
+    // For example, you can make an API call or include it in a form submission
+    // Replace the following line with your backend communication code
+    console.log("Selected Cells:", selectedCells);
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.top}></div>
@@ -25,9 +36,8 @@ const Editor: React.FC = () => {
       </div>
       <div className={styles.formContainer}>
         <div className={styles.create}>1</div>
-        {Array.from({ length: 4 }, (_, colIndex) => (
-        <div key={colIndex} className={styles.gridItem}>
-          {Array.from({ length: 10 }, (_, colIndex) => (
+        <div className={styles.gridItem}>
+          {Array.from({ length: 40 }, (_, colIndex) => (
             <div key={colIndex} className={styles.column}>
               {Array.from({ length: 6 }, (_, rowIndex) => (
                 <div
@@ -41,8 +51,8 @@ const Editor: React.FC = () => {
             </div>
           ))}
         </div>
-        ))}
       </div>
+      <button onClick={saveSelectedCells}>Save Selected Cells</button>
     </div>
   );
 };
