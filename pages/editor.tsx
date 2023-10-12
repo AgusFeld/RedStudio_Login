84c81afe05efa3152bd6ebdb46e426ffaf54cdd2
AgusFeld <101400409+AgusFeld@users.noreply.github.com>
2023-10-12 10:45:36 -0300

    Independant Grids

--- pages/editor.tsx
+++ pages/editor.tsx
@@ -2,11 +2,22 @@ import React, { useState } from "react";
 import styles from "./editor.module.css";
 import { Howl, Howler } from "howler";
 
-
 const Editor: React.FC = () => {
   // Estado para almacenar las filas seleccionadas en la cuadrícula
-  const [selectedRows, setSelectedRows] = useState<Array<number | null>>(
-    Array.from({ length: 40 }, () => null) // Inicialmente todas las filas están deseleccionadas (null)
+  const [selectedRows1, setSelectedRows1] = useState<Array<number | null>>(
+    Array.from({ length: 40 }, () => null)
+  );
+  const [selectedRows2, setSelectedRows2] = useState<Array<number | null>>(
+    Array.from({ length: 40 }, () => null)
+  );
+  const [selectedRows3, setSelectedRows3] = useState<Array<number | null>>(
+    Array.from({ length: 40 }, () => null)
+  );
+  const [selectedRows4, setSelectedRows4] = useState<Array<number | null>>(
+    Array.from({ length: 40 }, () => null)
+  );
+  const [selectedRows5, setSelectedRows5] = useState<Array<number | null>>(
+    Array.from({ length: 40 }, () => null)
   );
 
   // Definir los sonidos disponibles para cada fila
@@ -16,52 +27,58 @@ const Editor: React.FC = () => {
     "sound3.mp3",
     "sound4.mp3",
     "sound5.mp3",
-    "sound6.mp3"
+    "sound6.mp3",
   ];
 
-  // Manejar la selección de filas en la cuadrícula
-  const handleRowSelect = (colIndex: number, rowIndex: number) => {
-    // Crear una copia del array selectedRows
-    const newSelectedRows = [...selectedRows];
-    // Establecer la fila seleccionada en la posición colIndex
-    newSelectedRows[colIndex] = rowIndex;
-    // Actualizar el estado selectedRows con la nueva selección
-    setSelectedRows(newSelectedRows);
+  const handleRowSelect = (setSelectedRows: React.Dispatch<React.SetStateAction<(number | null)[]>>, colIndex: number, rowIndex: number) => {
+    setSelectedRows((prevSelectedRows) => {
+      prevSelectedRows[colIndex] = rowIndex;
+      return [...prevSelectedRows];
+    });
   };
+  
 
   // Función para convertir los valores de las celdas en nombres de sonido
-  const convertCellsToSounds = () => {
-    // Mapear las filas seleccionadas a nombres de archivo de sonido
+  const convertCellsToSounds = (selectedRows: Array<number | null>) => {
     const selectedSounds = selectedRows.map((rowIndex) => {
-      // Verificar si rowIndex es válido y dentro de los límites de sounds
       if (rowIndex !== null && rowIndex >= 0 && rowIndex < sounds.length) {
-        return sounds[rowIndex]; // Devolver el nombre del sonido correspondiente
+        return sounds[rowIndex];
       }
-      return ""; // Devolver una cadena vacía si no hay una selección válida
+      return "";
     });
-    return selectedSounds; // Devolver un array de nombres de sonido
+    return selectedSounds;
   };
 
   // Función para reproducir la pista de música
   const playMusicTrack = () => {
-    const selectedSounds = convertCellsToSounds(); // Obtener los nombres de sonido seleccionados
+    const selectedSounds1 = convertCellsToSounds(selectedRows1);
+    const selectedSounds2 = convertCellsToSounds(selectedRows2);
+    const selectedSounds3 = convertCellsToSounds(selectedRows3);
+    const selectedSounds4 = convertCellsToSounds(selectedRows4);
+    const selectedSounds5 = convertCellsToSounds(selectedRows5);
 
-    // Crear un array para almacenar objetos Howl para cada sonido seleccionado
-    const soundObjects = selectedSounds.map((soundFile) => new Howl({ src: [soundFile] }));
-    
-    // Función para reproducir los sonidos secuencialmente
-    const playNextSound = (index: number) => {
-      // Verificar si index está dentro de los límites del array soundObjects
+    // Create Howl objects for each sound selected
+    const soundObjects1 = selectedSounds1.map((soundFile) => new Howl({ src: [soundFile] }));
+    const soundObjects2 = selectedSounds2.map((soundFile) => new Howl({ src: [soundFile] }));
+    const soundObjects3 = selectedSounds3.map((soundFile) => new Howl({ src: [soundFile] }));
+    const soundObjects4 = selectedSounds4.map((soundFile) => new Howl({ src: [soundFile] }));
+    const soundObjects5 = selectedSounds5.map((soundFile) => new Howl({ src: [soundFile] }));
+
+    // Function to play sounds sequentially
+    const playNextSound = (soundObjects: Howl[], index: number) => {
       if (index < soundObjects.length) {
-        soundObjects[index].play(); // Reproducir el sonido en la posición index
-        // Configurar un evento "end" para reproducir el siguiente sonido cuando el actual termine
+        soundObjects[index].play();
         soundObjects[index].on("end", () => {
-          playNextSound(index + 1);
+          playNextSound(soundObjects, index + 1);
         });
       }
     };
-    
-    playNextSound(0); // Comenzar a reproducir el primer sonido en la secuencia
+
+    playNextSound(soundObjects1, 0);
+    playNextSound(soundObjects2, 0);
+    playNextSound(soundObjects3, 0);
+    playNextSound(soundObjects4, 0);
+    playNextSound(soundObjects5, 0);
   };
 
   return (
@@ -79,10 +96,64 @@ const Editor: React.FC = () => {
               {Array.from({ length: 6 }, (_, rowIndex) => (
                 <div
                   key={rowIndex}
-                  className={`${styles.cell} ${
-                    selectedRows[colIndex] === rowIndex ? styles.selected : ""
-                  }`}
-                  onClick={() => handleRowSelect(colIndex, rowIndex)}
+                  className={`${styles.cell} ${selectedRows1[colIndex] === rowIndex ? styles.selected : ""}`}
+                  onClick={() => handleRowSelect(setSelectedRows1, colIndex, rowIndex)}
+                ></div>
+              ))}
+            </div>
+          ))}
+        </div>
+        <div className={styles.create}>2</div>
+        <div className={styles.gridItem}>
+          {Array.from({ length: 40 }, (_, colIndex) => (
+            <div key={colIndex} className={styles.column}>
+              {Array.from({ length: 6 }, (_, rowIndex) => (
+                <div
+                  key={rowIndex}
+                  className={`${styles.cell} ${selectedRows2[colIndex] === rowIndex ? styles.selected : ""}`}
+                  onClick={() => handleRowSelect(setSelectedRows2, colIndex, rowIndex)}
+                ></div>
+              ))}
+            </div>
+          ))}
+        </div>
+        <div className={styles.create}>3</div>
+        <div className={styles.gridItem}>
+          {Array.from({ length: 40 }, (_, colIndex) => (
+            <div key={colIndex} className={styles.column}>
+              {Array.from({ length: 6 }, (_, rowIndex) => (
+                <div
+                  key={rowIndex}
+                  className={`${styles.cell} ${selectedRows3[colIndex] === rowIndex ? styles.selected : ""}`}
+                  onClick={() => handleRowSelect(setSelectedRows3, colIndex, rowIndex)}
+                ></div>
+              ))}
+            </div>
+          ))}
+        </div>
+        <div className={styles.create}>4</div>
+        <div className={styles.gridItem}>
+          {Array.from({ length: 40 }, (_, colIndex) => (
+            <div key={colIndex} className={styles.column}>
+              {Array.from({ length: 6 }, (_, rowIndex) => (
+                <div
+                  key={rowIndex}
+                  className={`${styles.cell} ${selectedRows4[colIndex] === rowIndex ? styles.selected : ""}`}
+                  onClick={() => handleRowSelect(setSelectedRows4, colIndex, rowIndex)}
+                ></div>
+              ))}
+            </div>
+          ))}
+        </div>
+        <div className={styles.create}>5</div>
+        <div className={styles.gridItem}>
+          {Array.from({ length: 40 }, (_, colIndex) => (
+            <div key={colIndex} className={styles.column}>
+              {Array.from({ length: 6 }, (_, rowIndex) => (
+                <div
+                  key={rowIndex}
+                  className={`${styles.cell} ${selectedRows5[colIndex] === rowIndex ? styles.selected : ""}`}
+                  onClick={() => handleRowSelect(setSelectedRows5, colIndex, rowIndex)}
                 ></div>
               ))}
             </div>
