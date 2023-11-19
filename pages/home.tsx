f961d517019bbe6e8bf6aeb1656c01c9bfab29b7
AgusFeld <101400409+AgusFeld@users.noreply.github.com>
2023-11-19 17:37:57 -0300

    home (sin conectar con Listen)

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,16 +1,62 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useRouter } from "next/router";
-import styles from "./home.module.css"
+import styles from "./home.module.css";
+import axios from 'axios';
 
 const Home: React.FC = () => {
 
     const [selectedImages, setSelectedImages] = useState([false, false, false, false, false]);
+    const [songs, setSongs] = useState<Song[]>([]);
+    const [filteredSongs, setFilteredSongs] = useState<Song[]>([]);
+    const router = useRouter();
 
-    const toggleSelect = (index : any) => {
+
+    const toggleSelect = (index: number) => {
         const updatedSelectedImages = [...selectedImages];
         updatedSelectedImages[index] = !selectedImages[index];
         setSelectedImages(updatedSelectedImages);
+    
+        const selectedGenres = updatedSelectedImages.reduce(
+          (acc: string[], isSelected: boolean, i: number) => {
+            if (isSelected) {
+              // Map de índices a géneros según el orden establecido en la interfaz
+              const genres = ['Jazz', 'Blues', 'Disco', 'Rock', 'Reggae'];
+              acc.push(genres[i]);
+            }
+            return acc;
+          },
+          []
+        );
+    
+        if (selectedGenres.length > 0) {
+          const filteredSongs = songs.filter((song) => selectedGenres.includes(song.genre));
+          setFilteredSongs(filteredSongs);
+        } else {
+          // Si no hay géneros seleccionados, mostrar todas las canciones
+          setFilteredSongs(songs);
+        }
+      };
+
+    interface Song {
+        name: string;
+        genre: string;
+      }
+
+    const fetchUserSongs = async () => {
+        try {
+        const response = await axios.get('/api/music'); // Ruta a tu API que devuelve las canciones del usuario
+        if (response.data) {
+            setSongs(response.data);
+        }
+        } catch (error) {
+        console.error('Error fetching user songs:', error);
+        }
     };
+
+    useEffect(() => {
+        // Llamar a la función para obtener las canciones del usuario cuando el componente se monte
+        fetchUserSongs();
+    }, []);
 
     return(
 
@@ -52,23 +98,15 @@ const Home: React.FC = () => {
                             <div className={styles.overflowContainer}>
                                 {/* Contenido que quieres que sea desplazable */}
                                 <div className={styles.scrollableContent}>          
-                                    <div className={styles.song}>
+                                    {songs.map((song, index) => (
+                                        <div key={index} className={styles.song}>
                                         <button className={styles.subtract}>
                                             <img src="Subtract.png" alt="" />
                                         </button>
-                                    </div>
-                                    <div className={styles.song}></div>
-                                    <div className={styles.song}></div>
-                                    <div className={styles.song}></div>
-                                    <div className={styles.song}></div>
-                                    <div className={styles.song}></div>
-                                    <div className={styles.song}></div>
-                                    <div className={styles.song}></div>
-                                    <div className={styles.song}></div>
-                                    <div className={styles.song}></div>
-                                    <div className={styles.song}></div>
-                                    <div className={styles.song}></div>
-                                    <div className={styles.song}></div>
+                                        <div>Nombre: {song.name}</div>
+                                        <div>Género: {song.genre}</div>
+                                        </div>
+                                    ))}
                                 </div>
                             </div>
                         </div>
